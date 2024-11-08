24584b921d5205f16b742d7d17b9b5379e322391
Shreyans Shah <121716408+Shreyansh055@users.noreply.github.com>
2024-11-08 14:51:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mdx from '@mdx-js/rollup';
 
 export default defineConfig({
+  base: '/Me_Shreyy/', // GitHub repo name
   plugins: [
     { enforce: 'pre', ...mdx() },
     react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
   ],
-})
+});
